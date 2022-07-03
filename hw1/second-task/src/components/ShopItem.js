c6b799309073218219b9706abbf1436f46dd1ff7
Корицкий Александр Юрьевич <AYKoritskiy@sberbank.ru>
2022-07-03 14:57:20 +0300

    [hw1] add second task

---
+++ hw1/second-task/src/components/ShopItem.js
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import ShopItemModel from "../models/ShopItemModel";
+import React from "react";
+
+class ShopItem extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const {item} = this.props;
+
+        return (
+            <div className="main-content">
+                <h2>{item.brand}</h2>
+                <h1>{item.title}</h1>
+                <h3>{item.description}</h3>
+                <div className="description">
+                    {item.descriptionFull}
+                </div>
+                <div className="highlight-window mobile">
+                    <div className="highlight-overlay"></div>
+                </div>
+                <div className="divider"></div>
+                <div className="purchase-info">
+                    <div className="price">{item.currency}{item.price.toFixed(2)}</div>
+                    <button>Добавить в корзину</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+ShopItem.propTypes = {
+    item: PropTypes.instanceOf(ShopItemModel).isRequired
+}
+
+export default ShopItem;
